36d70166a29607432f05c25f6d37df285895017a
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2019-03-19 23:39:13 -0700

    update players choice on the database

--- assets/js/game.js
+++ assets/js/game.js
@@ -15,27 +15,25 @@ $(document).ready(function() {
 
   var p1 = null;
   var p2 = null;
-  var turn = { turn_key: 1 };
-
+  var turn = 1;
   var yourPlayerName = '';
+  var choice = '';
 
   // get players data
   var getPlayers = function() {
     database.ref('/players').on('value', function(snapshot) {
-      console.log('snapshot', snapshot.val());
-
       // Check if player 1 exists in the database
       if (snapshot.child('p1').exists()) {
         // Record player1 data
         p1 = snapshot.val().p1;
-
+        // Update player1 display
         $('#user-label').text(p1.name);
         $('#user-score').text(p1.win);
       } else {
         p1 = null;
 
         // Update player1 display
-        $('.waiting-notice').text('Waiting for Player 1...');
+        $('#user-label').text('Waiting for Player 1...');
 
         database.ref('/outcome').remove();
       }
@@ -43,7 +41,7 @@ $(document).ready(function() {
       // Check for existence of player 2 in the database
       if (snapshot.child('p2').exists()) {
         // Record player2 data
-        p2 = snapshot.val().player2;
+        p2 = snapshot.val().p2;
         // Update player2 display
         $('#comp-label').text(p2.name);
         $('#comp-score').text(p2.win);
@@ -51,7 +49,7 @@ $(document).ready(function() {
         p2 = null;
 
         // Update player2 display
-        $('.waiting-notice').text('Waiting for Player 2...');
+        $('#comp-label').text('Waiting for Player 2...');
 
         database.ref('/outcome').remove();
       }
@@ -61,12 +59,36 @@ $(document).ready(function() {
         $('.waiting-notice').text('Waiting on ' + p1.name + ' to choose...');
       }
 
-      // If both players leave the game, empty the chat session
+      // If both players leave the game
       if (!p1 && !p1) {
         database.ref('/turn').remove();
         database.ref('/outcome').remove();
 
         $('.waiting-notice').text('');
+      }
+    });
+  };
+
+  // get turn data
+  var getTurn = function() {
+    database.ref('/turn').on('value', function(snapshot) {
+      if (snapshot.val()) {
+        if (snapshot.val() === 1) {
+          turn = 1;
+
+          if (p1 && p2) {
+            $('.waiting-notice').text(
+              'Waiting on ' + p1.name + ' to choose...'
+            );
+          }
+        } else if (snapshot.val() === 2) {
+          turn = 2;
+          if (p1 && p2) {
+            $('.waiting-notice').text(
+              'Waiting on ' + p2.name + ' to choose...'
+            );
+          }
+        }
       }
     });
   };
@@ -98,7 +120,7 @@ $(document).ready(function() {
           .child('/players/p1')
           .set(p1);
 
-        turn = { turn_key: 1 };
+        turn = 1;
 
         database
           .ref()
@@ -121,7 +143,6 @@ $(document).ready(function() {
           choice: ''
         };
 
-        // write p2 aka the above object to the database
         database
           .ref()
           .child('/players/p2')
@@ -133,14 +154,41 @@ $(document).ready(function() {
           .remove();
       }
 
-      $('#user-label').text(yourPlayerName);
-
       $('.user-info').fadeOut();
     }
   });
 
-  // On-click function for selecting a move.
-  $('.choice').on('click', function() {});
+  //player 1 click handler
+  $('#p1').on('click', '.choice', function() {
+    if (p1 && p2 && yourPlayerName == p1.name) {
+      choice = $(this).attr('id');
+
+      database
+        .ref()
+        .child('/players/p1/choice')
+        .set(choice);
+
+      turn = 2;
+
+      database
+        .ref()
+        .child('/turn')
+        .set(turn);
+    }
+  });
+
+  //player 2 click handler
+  $('#p2').on('click', '.choice', function() {
+    if (p1 && p2 && yourPlayerName == p2.name) {
+      choice = $(this).attr('id');
+
+      database
+        .ref()
+        .child('/players/p2/choice')
+        .set(choice);
+    }
+  });
 
   getPlayers();
+  getTurn();
 });
