711c1410beff16dc6c21199595fc7505a7a09aa4
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2019-03-20 17:16:22 -0700

    add firebase config file

---
+++ assets/js/config.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+  apiKey: 'AIzaSyBBgGNxd_SS45NxKnl0r9TZG1rDtJ5Ub1w',
+  authDomain: 'rock-paper-scissors-f9ed2.firebaseapp.com',
+  databaseURL: 'https://rock-paper-scissors-f9ed2.firebaseio.com',
+  projectId: 'rock-paper-scissors-f9ed2',
+  storageBucket: '',
+  messagingSenderId: '378816581305'
+};
+
+firebase.initializeApp(config);
--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,4 @@
 $(document).ready(function() {
-  // Initialize Firebase
-  var config = {
-    apiKey: 'AIzaSyBBgGNxd_SS45NxKnl0r9TZG1rDtJ5Ub1w',
-    authDomain: 'rock-paper-scissors-f9ed2.firebaseapp.com',
-    databaseURL: 'https://rock-paper-scissors-f9ed2.firebaseio.com',
-    projectId: 'rock-paper-scissors-f9ed2',
-    storageBucket: '',
-    messagingSenderId: '378816581305'
-  };
-  firebase.initializeApp(config);
-
   // Create a variable to reference the database
   var database = firebase.database();
 
